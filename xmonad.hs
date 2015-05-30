03039587517bd5c96e2a756c52da41188fa7815d
sshihata <samy.s.shihata@gmail.com>
2015-05-30 23:59:01 +0200

    [auto update]

--- xmonad.hs
+++ xmonad.hs
@@ -1,46 +1,90 @@
 import XMonad
-import XMonad.Util.Paste
-import XMonad.Util.EZConfig
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
-import XMonad.Util.Run
-import XMonad.Hooks.ManageHelpers
-import Control.Monad (liftM2)
-import XMonad.Actions.CycleWS
-import XMonad.Hooks.SetWMName
-import XMonad.Layout.NoBorders
-import qualified XMonad.StackSet as W
 
--- Custom Manage Hook
-customManageHook :: ManageHook
-customManageHook = composeAll
-    [ (role =? "gimp-toolbox" <||> role =? "gimp-image-window") --> (ask >>= doF . W.sink)
-    , className =? "Gimp" --> viewShift "5>gimp"
-    , isFullscreen --> (doF W.focusDown <+> doFullFloat)
-    ] where
-        role = stringProperty "WM_WINDOW_ROLE" 
-        viewShift = doF . liftM2 (.) W.greedyView W.shift
+import XMonad.Prompt
+import XMonad.Prompt.RunOrRaise (runOrRaisePrompt)
+import XMonad.Prompt.AppendFile (appendFilePrompt)
+
+import System.IO
+import System.Exit
+import XMonad.Operations
+
+import XMonad.Util.EZConfig
+import XMonad.Util.Run
+import XMonad.Actions.CycleWS
+
+import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.SetWMName
+import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.UrgencyHook
+import XMonad.Hooks.FadeInactive
+import XMonad.Hooks.EwmhDesktops
+
+import XMonad.Layout.NoBorders
+import XMonad.Layout.PerWorkspace (onWorkspace, onWorkspaces)
+import XMonad.Layout.Reflect (reflectHoriz)
+import XMonad.Layout.IM
+import XMonad.Layout.SimpleFloat
+import XMonad.Layout.Spacing
+import XMonad.Layout.ResizableTile
+import XMonad.Layout.LayoutHints
+import XMonad.Layout.LayoutModifier
+import XMonad.Layout.Grid
+
+import Data.Ratio ((%))
+
+import qualified XMonad.StackSet as W
+import qualified Data.Map as M
+
+cTerminal = "urxvt"
+cBorderWidth = 1
+cFocusedBorderColor = "dark gray"
+cNormalBorderColor = "black"
+cWorkspaces    = ["1","2","3","4","5","6","7","8","9"]
+cBitmapsDir = "/home/sshehata/.xmonad/icons"
+cManageHook = composeAll
+  [ isFullscreen --> (doF W.focusDown <+> doFullFloat)
+  ]
+
+cLogHook :: Handle -> X ()
+cLogHook h = dynamicLogWithPP $ defaultPP
+  {
+    ppUrgent            =   dzenColor "#ff0000" "black"
+    , ppWsSep             =   ""
+    , ppLayout            =   dzenColor "#ff0000" "black" .
+    (\x -> case x of
+     "Tall"             ->      "^i(" ++ cBitmapsDir ++ "/tall.xpm)"
+     "Mirror Tall"      ->      "^i(" ++ cBitmapsDir ++ "/mtall.xpm)"
+     "Full"                      ->      "^i(" ++ cBitmapsDir ++ "/full.xpm)"
+     "Simple Float"              ->      "~"
+     _                           ->      x
+    )
+    , ppTitle             =   ("" ++) . dzenColor "green" "black" . dzenEscape
+    , ppOutput            =   hPutStrLn h
+  }
+
+cXmonadBar = "dzen2 -x '0' -y '0'  -w '640' -ta 'l' "
+cStatusBar = "conky | dzen2 -x '640' -w '1040' -ta 'r' -y '0'"
 
 main = do
-   xmproc <- spawnPipe "/usr/bin/xmobar /home/sshihata/.xmobarrc"
-   xmonad $ defaultConfig
-	{ manageHook = customManageHook <+> manageDocks <+> manageHook defaultConfig
-	, layoutHook = avoidStruts $ smartBorders $ layoutHook defaultConfig
-	, logHook = dynamicLogWithPP xmobarPP
-				{ ppOutput = hPutStrLn xmproc
-				, ppTitle = xmobarColor "green" "" . shorten 50
-				}
-	, terminal    = "urxvt"
-	, borderWidth = 1
-	, focusedBorderColor = "light blue"
-  , startupHook = setWMName "LG3D"
-	} `additionalKeysP`	
-	[ (("<XF86AudioLowerVolume>"), spawn "amixer -q set Master unmute && amixer -q set Master 1%-")
-	, (("<XF86AudioRaiseVolume>"), spawn "amixer -q set Master unmute && amixer -q set Master 1%+")
-	, (("<XF86AudioMute>"), spawn "amixer -q set Master toggle")
-	, (("M-<Down>"), nextWS)
-	, (("M-<Up>"), prevWS)
-	, (("M-<Right>"), moveTo Next EmptyWS)
-	, (("M-<Tab>"), toggleWS)
-	, (("M-<Print>"), spawn "scrot ~/screen_%Y-%m-%d-%H-%M-%S.png -d 1")
-	]
+   dzenLeftBar <- spawnPipe cXmonadBar
+   dzenRightBar <- spawnPipe cStatusBar
+   xmonad $ defaultConfig {
+     terminal = cTerminal,
+     borderWidth = cBorderWidth,
+     focusedBorderColor = cFocusedBorderColor,
+     normalBorderColor = cNormalBorderColor,
+     manageHook = cManageHook <+> manageDocks <+> manageHook defaultConfig,
+     layoutHook = avoidStruts $ smartBorders $ layoutHook defaultConfig,
+     logHook = cLogHook dzenLeftBar >> fadeInactiveLogHook 0xdddddddd
+     } `additionalKeysP`
+     [ (("XF86AudioLowerVolume>"), spawn "amixer -q set Master unmute && amixer -q set Master 1%-")
+     , (("<XF86AudioRaiseVolume>"), spawn "amixer -q set Master unmute && amixer -q set Master 1%+")
+     , (("<XF86AudioMute>"), spawn "amixer -q set Master toggle")     
+     , (("M-<Down>"), nextWS)
+     , (("M-<Up>"), prevWS)
+     , (("M-<Right>"), moveTo Next EmptyWS)
+     , (("M-<Left>"), moveTo Prev NonEmptyWS)
+     , (("M-<Tab>"), toggleWS)
+     , (("M-<Print>"), spawn "scrot ~/screen_%Y-%m-%d-%H-%M-%S.png -d 1")
+     ]
